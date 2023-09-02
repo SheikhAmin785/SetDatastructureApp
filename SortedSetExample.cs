e59ce3fb444b2b26787b2a47a86f3c0bb9d33b05
MD.AL-AMIN <143488695+SheikhAmin785@users.noreply.github.com>
2023-09-02 22:22:36 +0600

    Add files via upload

---
+++ SortedSetExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableExampleApp
+{
+    class SortedSetExample
+    {
+        static void Main()
+        {
+            SortedSet<string> bandsBradLikes =
+            new SortedSet<string>(new[] {
+"Manowar", "Blind Guardian", "Dio", "Kiss",
+"Dream Theater", "Megadeth", "Judas Priest",
+"Kreator", "Iron Maiden", "Accept"
+            });
+            SortedSet<string> bandsAngelinaLikes =
+            new SortedSet<string>(new[] {
+"Iron Maiden", "Dio", "Accept", "Manowar", "Slayer",
+"Megadeth", "Running Wild", "Grave Gigger", "Metallica"
+            });
+            Console.Write("Brad Pitt likes these bands: ");
+            Console.WriteLine(string.Join(", ", bandsBradLikes));
+            Console.Write("Angelina Jolie likes these bands: ");
+            Console.WriteLine(string.Join(", ", bandsAngelinaLikes));
+            SortedSet<string> intersectBands =
+            new SortedSet<string>(bandsBradLikes);
+            intersectBands.IntersectWith(bandsAngelinaLikes);
+            Console.WriteLine(string.Format(
+            "Does Brad Pitt like Angelina Jolie? {0}",
+            intersectBands.Count >= 5 ? "Yes!" : "No!"));
+            Console.Write(
+            "Because Brad Pitt and Angelina Jolie both like: ");
+            Console.WriteLine(string.Join(", ", intersectBands));
+            SortedSet<string> unionBands =
+            new SortedSet<string>(bandsBradLikes);
+            unionBands.UnionWith(bandsAngelinaLikes);
+            Console.Write(
+            "All bands that Brad Pitt or Angelina Jolie like: ");
+            Console.WriteLine(string.Join(", ", unionBands));
+        }
+    }
+}
+}
