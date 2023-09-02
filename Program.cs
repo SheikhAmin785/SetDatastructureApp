3a45751bb3adc6ded32b3ab71eaa13e4a66e0d47
MD.AL-AMIN <143488695+SheikhAmin785@users.noreply.github.com>
2023-09-02 22:21:35 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableExampleApp
+{
+
+   public static void Main()
+    {
+        Hashtable mytable = new Hashtable();
+
+        mytable.Add("name", "alamin");
+        mytable.Add("rollnumber", 12);
+        mytable.Add("address", "miami");
+        Console.WriteLine(mytable["rollnumber"]);
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
